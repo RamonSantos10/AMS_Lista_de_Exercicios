10487ee1dd2342b53cdbe7f9de3e8312390b4d8f
Ramon dos Santos <ramontedescodossantos2020@gmail.com>
2022-05-26 05:37:33 -0300

    Exercicios_Operadores

--- Exercicio_4/Celsius_Fahrenheit/Program.cs
+++ Exercicio_4/Celsius_Fahrenheit/Program.cs
@@ -4,6 +4,6 @@ int fahrenheit = 0;
 Console.Write("Digite o valor em °C: ");
 celsius = Convert.ToInt32(Console.ReadLine());
 
-fahrenheit = ((9 * celsius + 160) / 5);
+fahrenheit = (9 * celsius + 160) / 5;
 
 Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
