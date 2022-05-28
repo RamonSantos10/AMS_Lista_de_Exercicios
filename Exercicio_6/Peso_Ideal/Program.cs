2b44302e03ba8d8e81b8f71a25eb9fd30eac743d
Ramon dos Santos <ramontedescodossantos2020@gmail.com>
2022-05-28 10:26:18 -0300

    Exercicios_Operadores

--- Exercicio_6/Peso_Ideal/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿double altura = 0;
-double pIdeal = 0;
-
-Console.Write("Digite sua altura em metros: ");
-altura = Convert.ToDouble(Console.ReadLine());
-
-pIdeal = ((72.7 * altura) - 58);
-
-Console.WriteLine($"Seu peso ideal é: {pIdeal}Kg");
